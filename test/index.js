3aad490a368366d2de9f23b8ae5cc2fcb74906d0
Shuvalov Anton <anton_shuvalov@me.com>
2014-09-28 01:27:13 +0400

    update tests to 1.0.0

--- test/index.js
+++ test/index.js
@@ -5,11 +5,8 @@ var html = '<html><body><h1>Hello World!</h1><p class="hello">Heya Big World!</b
 var $ = require('jquery');
 var watcherFabr = require('../index');
 
-var obj = {};
-var deferred = null;
-var Watcher = null;
-
 describe('Watcher', function () {
+  var Watcher;
   before(function (done) {
     env(html, function (err, window) {
       $ = $(window);
@@ -17,25 +14,32 @@ describe('Watcher', function () {
       done(err);
     });
   });
-
+  
   it('should be constructor', function () {
     Watcher.should.be.type('function');
   });
 
-  it('should reject defered', function (done) {
-     deferred = new Watcher({ observable: obj, counter: 3, interval: 100 });
-     deferred.fail(function() { done(); });
+  describe('timeout', function () {
+    var deferred;
+    it('should reject defered', function (done) {
+       deferred = new Watcher({ counter: 3, interval: 100 });
+       deferred.fail(function() { done(); });
+    });
   });
 
-  it('should resolve deferred manualy', function (done) {
-    deferred = new Watcher({ observable: obj, counter: 5, interval: 100 });
-    deferred
-      .progress(function (obj, deferred) {
-        if ('prop' in obj) { deferred.resolve(); }
-      })
-      .done(function(){ done(); });
-    setTimeout(function () {
-      obj.prop = 'fuck yeah!11';
-    }, 200);
+  describe('resolve', function() {
+    var obj = {};
+    var deferred;
+    it('should resolve deferred manualy', function (done) {
+      deferred = new Watcher({ counter: 5, interval: 100 });
+      deferred
+        .progress(function (deferred) {
+          if ('prop' in obj) { deferred.resolve(); }
+        })
+        .done(function(){ done(); });
+      setTimeout(function () {
+        obj.prop = 'fuck yeah!11';
+      }, 200);
+    });
   });
 });
